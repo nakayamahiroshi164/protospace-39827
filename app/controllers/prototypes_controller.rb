45b0d8c32766044ae4f30b458bf7aedb50663444
nakayamahiroshi164 <box.xx164@gmail.com>
2023-09-24 13:25:10 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+  
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @user = current_user
+    @prototype = Prototype.new(prototype_params)
+    @prototype.user = @user
+
+    if @prototype.save
+      redirect_to prototypes_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to '/'
+  end
+
+  
+  private  
+  def prototype_params  
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  
+  def authorize_user!
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path, alert: "権限がありません。"
+    end
+  end
+end
