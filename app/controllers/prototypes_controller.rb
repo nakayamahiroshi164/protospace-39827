f7bf4660ed33d7489350653a2abef7ee1eafd250
nakayamahiroshi164 <box.xx164@gmail.com>
2023-09-25 13:15:31 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authorize_user!, only: [:edit, :update, :destroy]
   
   def index
     @prototypes = Prototype.all
